a23c85d6b987cc06d3fad618617de14af72a9396
mdgrogan <grogan2122@gmail.com>
2018-04-23 00:27:32 -0500

    ...

--- evaluate/AOD/test.py
+++ evaluate/AOD/test.py
@@ -24,18 +24,29 @@ def EditFcnProto(templateFile, height, width):
         with open(outFile, 'w') as fd:
             fd.write(template.format(height=height,width=width))
 
+def CLAHE(bgr, lim, gs):
+    lab = cv2.cvtColor(bgr, cv2.COLOR_BGR2LAB)
+    lab_planes = cv2.split(lab)
+    clahe = cv2.createCLAHE(clipLimit=lim, tileGridSize=(gs, gs))
+    lab_planes[0] = clahe.apply(lab_planes[0])
+    lab = cv2.merge(lab_planes)
+    bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+    return bgr
+
+
 def test():
     #caffe.set_mode_gpu()
     #caffe.set_device(0)
     caffe.set_mode_cpu();
 
     imagesnum=0;
-    for i in range(1, 10):
+    for i in range(1, 19):
         imagesnum = imagesnum + 1;
-        npstore = caffe.io.load_image('../img/%s.jpg'% str(i))
-        label = caffe.io.load_image('../img/%s-clear.jpg'% str(i))
+        npstore = caffe.io.load_image('../img/%s.png'% str(i))
+        label = caffe.io.load_image('../img/%s-clear.png'% str(i))
         height = npstore.shape[0]
         width = npstore.shape[1]
+
 
         templateFile = 'test_template.prototxt'
         EditFcnProto(templateFile, height, width)
@@ -56,13 +67,34 @@ def test():
         net.blobs['label'].data[...] = batchdata1
 
         net.forward();
+        print('iteration' + str(i))
 
         data = net.blobs['sum'].data[0];
         data = data.transpose((1, 2, 0));
         data = data[:, :, ::-1]
 
-        savepath = '../img/' + str(i) + '_AOD-Net.jpg'
-        cv2.imwrite(savepath, data * 255.0,[cv2.IMWRITE_JPEG_QUALITY, 100])
+        savepath = '../img/' + str(i) + '_AOD-Net.png'
+        cv2.imwrite(savepath, data * 255.0)
+
+        # my gut says this isn't good
+        data = data*255.0
+        data = data.astype(np.uint8)
+
+        data1 = CLAHE(data, 0.6, 8)
+        savepath = '../img/' + str(i) + '_CLAHE-0.6-8.png'
+        cv2.imwrite(savepath, data1)
+
+        data2 = CLAHE(data, 0.4, 8)
+        savepath = '../img/' + str(i) + '_CLAHE-0.4-8.png'
+        cv2.imwrite(savepath, data2)
+
+        data3 = CLAHE(data, 0.6, 12)
+        savepath = '../img/' + str(i) + '_CLAHE-0.6-12.png'
+        cv2.imwrite(savepath, data3)
+
+        data4 = CLAHE(data, 0.4, 12)
+        savepath = '../img/' + str(i) + '_CLAHE-0.4-12.png'
+        cv2.imwrite(savepath, data4)
 
         f = open('loss.txt', 'a')
         f.write('{0:d} '.format(i))
--- evaluate/evaluate.m
+++ evaluate/evaluate.m
@@ -1,31 +1,91 @@
-for ii = 1:9
-   ground_truth = sprintf('img/%d_clear.jpg', ii);
-   hazy = sprintf('img/%d.jpg', ii);
-   AOD = sprintf('img/%d_AOD-Net.jpg', ii);
-   DARK = sprintf('img/%d_DarkChannel.jpg', ii);
-   AD = sprintf('img/%d_AOD->DARK.jpg', ii);
+p_H = 0.0;
+p_AOD = 0.0;
+p_I1 = 0.0;
+p_I2 = 0.0;
+p_I3 = 0.0;
+p_I4 = 0.0;
+s_H = 0.0;
+s_AOD = 0.0;
+s_I1 = 0.0;
+s_I2 = 0.0;
+s_I3 = 0.0;
+s_I4 = 0.0;
+
+for ii = 1:18
+   ground_truth = sprintf('img/%d-clear.png', ii);
+   hazy = sprintf('img/%d.png', ii);
+   l_AOD = sprintf('img/%d_AOD-Net.png', ii);
+   l_I1 = sprintf('img/%d_CLAHE-0.4-8.png', ii);
+   l_I2 = sprintf('img/%d_CLAHE-0.4-12.png', ii);
+   l_I3 = sprintf('img/%d_CLAHE-0.6-8.png', ii);
+   l_I4 = sprintf('img/%d_CLAHE-0.6-12.png', ii);
+
 
    L = imread(ground_truth);
    H = imread(hazy);
-   Im_AOD = imread(AOD);
-   Im_DARK = imread(DARK);
-   Im_AD = imread(AD);
-   
+   AOD = imread(l_AOD);
+   I1 = imread(l_I1);
+   I2 = imread(l_I2);
+   I3 = imread(l_I3);
+   I4 = imread(l_I4);
+
    [peaksnr, ~] = psnr(H, L); 
    fprintf('\n Haze PSNR(%d) = %0.4f', ii, peaksnr);
-   [peaksnr, ~] = psnr(Im_AOD, L); 
+   p_H = p_H + peaksnr;
+   [peaksnr, ~] = psnr(AOD, L); 
    fprintf('\n AOD PSNR(%d) = %0.4f', ii, peaksnr);
-   [peaksnr, ~] = psnr(Im_DARK, L); 
-   fprintf('\n DARK PSNR(%d) = %0.4f', ii, peaksnr);
-   [peaksnr, ~] = psnr(Im_AD, L); 
-   fprintf('\n AOD->DARK PSNR(%d) = %0.4f', ii, peaksnr);
+   p_AOD = p_AOD + peaksnr;
+   [peaksnr, ~] = psnr(I1, L); 
+   fprintf('\n CLAHE-0.4-8 PSNR(%d) = %0.4f', ii, peaksnr);
+   p_I1 = p_I1 + peaksnr;
+   [peaksnr, ~] = psnr(I2, L); 
+   fprintf('\n CLAHE-0.4-12 PSNR(%d) = %0.4f', ii, peaksnr);
+   p_I2 = p_I2 + peaksnr;
+   [peaksnr, ~] = psnr(I3, L); 
+   fprintf('\n CLAHE-0.6-8 PSNR(%d) = %0.4f', ii, peaksnr);
+   p_I3 = p_I3 + peaksnr;
+   [peaksnr, ~] = psnr(I4, L); 
+   fprintf('\n CLAHE-0.6-12 PSNR(%d) = %0.4f', ii, peaksnr);
+   p_I4 = p_I4 + peaksnr;
    
    [ssimval, ~] = ssim(H, L); 
    fprintf('\n Haze SSIM(%d) = %0.4f', ii, ssimval);
-   [ssimval, ~] = ssim(Im_AOD, L); 
+   s_H = s_H + ssimval;
+   [ssimval, ~] = ssim(AOD, L); 
    fprintf('\n AOD SSIM(%d) = %0.4f', ii, ssimval);
-   [ssimval, ~] = ssim(Im_DARK, L); 
-   fprintf('\n DARK SSIM(%d) = %0.4f', ii, ssimval);
-   [ssimval, ~] = ssim(Im_AD, L); 
-   fprintf('\n AOD->DARK SSIM(%d) = %0.4f\n', ii, ssimval);
+   s_AOD = s_AOD + ssimval;
+   [ssimval, ~] = ssim(I1, L); 
+   fprintf('\n CLAHE-0.4-8 SSIM(%d) = %0.4f', ii, ssimval);
+   s_I1 = s_I1 + ssimval;
+   [ssimval, ~] = ssim(I2, L); 
+   fprintf('\n CLAHE-0.6-12 SSIM(%d) = %0.4f', ii, ssimval);
+   s_I2 = s_I2 + ssimval;
+   [ssimval, ~] = ssim(I3, L); 
+   fprintf('\n CLAHE-0.4-8 SSIM(%d) = %0.4f', ii, ssimval);
+   s_I3 = s_I3 + ssimval;
+   [ssimval, ~] = ssim(I4, L); 
+   fprintf('\n CLAHE-0.6-12 SSIM(%d) = %0.4f\n', ii, ssimval);
+   s_I4 = s_I4 + ssimval;
 end
+
+fprintf('\n Haze PSNR = %0.4f', p_H/18);
+fprintf('\n AOD PSNR = %0.4f', p_AOD/18);
+fprintf('\n CLAHE-0.4-8 PSNR = %0.4f', p_I1/18);
+fprintf('\n CLAHE-0.4-12 PSNR = %0.4f', p_I2/18);
+fprintf('\n CLAHE-0.6-8 PSNR = %0.4f', p_I3/18);
+fprintf('\n CLAHE-0.6-12 PSNR = %0.4f', p_I4/18);
+fprintf('\n Haze SSIM = %0.4f', s_H/18);
+fprintf('\n AOD SSIM = %0.4f', s_AOD/18);
+fprintf('\n CLAHE-0.4-8 SSIM = %0.4f', s_I1/18);
+fprintf('\n CLAHE-0.6-12 SSIM = %0.4f', s_I2/18);
+fprintf('\n CLAHE-0.4-8 SSIM = %0.4f', s_I3/18);
+fprintf('\n CLAHE-0.6-12 SSIM = %0.4f\n', s_I4/18);
+
+
+
+
+
+
+
+
+
